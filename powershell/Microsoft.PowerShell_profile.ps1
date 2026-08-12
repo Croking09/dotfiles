6d033526cd913faabb396258ab730b181547241b
Javier HM <javierhm2004@gmail.com>
2026-08-12 13:04:26 +0200

    Added posh-git and customization

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -1,28 +1,42 @@
+Import-Module posh-git
+
+# ─────────────────────────────────────────────
+# Posh-git
+# ─────────────────────────────────────────────
+
+# Working directory
+$GitPromptSettings.DefaultPromptAbbreviateHomeDirectory = $true
+$GitPromptSettings.DefaultPromptPath.ForegroundColor = 'DeepSkyBlue'
+
+# Git
+$GitPromptSettings.BeforeStatus = '('
+$GitPromptSettings.AfterStatus = ')'
+$GitPromptSettings.BeforeStatus.ForegroundColor = 'LightGreen'
+$GitPromptSettings.AfterStatus.ForegroundColor = 'LightGreen'
+
+$GitPromptSettings.BranchColor.ForegroundColor = 'LightGreen'
+
+$GitPromptSettings.EnableFileStatus = $false
+
+# Prompt end
+$GitPromptSettings.DefaultPromptSuffix = ' $ '
+
+# ─────────────────────────────────────────────
+# Windows Terminal title prompt
+# ─────────────────────────────────────────────
+
 function global:prompt {
     $cwd = (Get-Location).ProviderPath
 
-    # Secuencia ANSI para cambiar el título de la ventana (compatible con Windows Terminal)
+    # Updates tab title in Windows Terminal
     $esc = [char]27
-    Write-Host "$esc]9;9;$cwd$esc\" -NoNewline  # Cambia también la información en pestañas
+    Write-Host "$esc]9;9;$cwd$esc\" -NoNewline
 
-    # Obtener rama Git si existe
-    $branch = ''
-    if (Test-Path .git) {
-        $branchName = & git rev-parse --abbrev-ref HEAD 2>$null
-        if ($branchName) {
-            # Color verde en ANSI (para Windows Terminal)
-            $branch = " $esc[32m($branchName)$esc[0m"
-        }
-    }
-
-    # Retornar el prompt final
-    "PS $cwd$branch> "
+    # Posh-git prompt
+    & $GitPromptScriptBlock
 }
 
-# Limpia la terminal y "oculta" el mensaje del nuevo powershell
-Clear-Host
-
-# Ejecutar fastfetch si está instalado
+# Fastfetch if instaled
 if (Get-Command fastfetch -ErrorAction SilentlyContinue) {
   fastfetch
 }
